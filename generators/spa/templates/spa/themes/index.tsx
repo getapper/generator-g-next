c60690a65347b014471f3e8088b8db2a256226ec
antoniogiordano <antonio.giordano@getapper.com>
2022-06-28 11:40:30 +0200

    responsive fonts MUI

--- generators/spa/templates/spa/themes/index.tsx
+++ generators/spa/templates/spa/themes/index.tsx
@@ -1,12 +1,14 @@
-import { createTheme } from "@mui/material";
+import { createTheme, responsiveFontSizes } from "@mui/material";
 
-export default createTheme({
-  palette: {
-    primary: {
-      main: "#71B9DA",
+export default responsiveFontSizes(
+  createTheme({
+    palette: {
+      primary: {
+        main: "#71B9DA",
+      },
+      secondary: {
+        main: "#6291A8",
+      },
     },
-    secondary: {
-      main: "#6291A8",
-    },
-  },
-});
+  })
+);
