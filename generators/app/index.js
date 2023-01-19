6bd130f734c19ecf6c20c77cce7f93d16c149103
antoniogiordano <antonio.giordano@getapper.com>
2023-01-19 11:40:45 +0100

    next app with next12 for compatibility

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,11 +3,6 @@ const Generator = require("yeoman-generator");
 
 module.exports = class extends Generator {
   install() {
-    this.spawnCommand("npx", [
-      "create-next-app@latest",
-      ".",
-      "--ts",
-      "--use-npm",
-    ]);
+    this.spawnCommand("npx", ["create-next-app@12", ".", "--ts", "--use-npm"]);
   }
 };
