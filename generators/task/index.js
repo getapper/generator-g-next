6d789726e5ab0d5900a957bc6a135018f5856bec
antoniogiordano <antonio.giordano@getapper.com>
2023-02-21 17:54:40 +0100

    Improved task command

--- generators/task/index.js
+++ generators/task/index.js
@@ -4,8 +4,11 @@ const chalk = require("chalk");
 const yosay = require("yosay");
 const kebabCase = require("kebab-case");
 const { pascalCase } = require("pascal-case");
-const path = require("path");
-const { requirePackages, copyEjsTemplateFolder } = require("../../common");
+const {
+  requirePackages,
+  copyEjsTemplateFolder,
+  checkPackageInstalled,
+} = require("../../common");
 
 module.exports = class extends Generator {
   initializing() {
@@ -57,6 +60,7 @@ module.exports = class extends Generator {
     copyEjsTemplateFolder(this, this.templatePath("./"), relativeToRootPath, {
       taskFunctionName,
       taskFolder,
+      isMongoInstalled: checkPackageInstalled(this, "mongodb"),
     });
 
     this.packageJson.merge({
