1291a99c74715faf5e2cf1d4528621b8ee0a0784
BoulosMansour <72452999+BoulosMansour@users.noreply.github.com>
2023-03-03 10:41:21 +0100

    delete function for generating random int (used for password generation)

--- generators/cookie-auth-role/index.js
+++ generators/cookie-auth-role/index.js
@@ -13,12 +13,6 @@ const {
 const { camelCase } = require("camel-case");
 const { snakeCase } = require("snake-case");
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-}
-
 const generateLibSessionFile = ({
   cookieRoles,
   projectName,
