a413ec1e017d9b29f8274def2eefcc08b6203094
antoniogiordano <antonio.giordano@getapper.com>
2023-05-19 15:28:33 +0200

    fixed app command with fixed packages versions

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,7 +4,7 @@ const Generator = require("yeoman-generator");
 module.exports = class extends Generator {
   install() {
     this.spawnCommand("npx", [
-      "create-next-app",
+      "create-next-app@13.2.3",
       ".",
       "--ts",
       "--use-npm",
@@ -14,6 +14,10 @@ module.exports = class extends Generator {
       "@/*",
       "--no-experimental-app",
       "--no-tailwind",
-    ]);
+    ]).on("exit", () => {
+      this.spawnCommand("npm", ["i", "next@13.2.3", "-S", "-E"]);
+      this.spawnCommand("npm", ["i", "eslint-config-next@13.2.3", "-S", "-E"]);
+      this.spawnCommand("npm", ["i", "typescript@4.9.5", "-S", "-E"]);
+    });
   }
 };
